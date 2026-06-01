3f4eee1202f41a178e257f876c54be9620f8786f
Hydra Pipeline <hydra-pipeline@conduction.nl>
2026-06-01 08:23:26 +0000

    fix(code-review bounded): Juan post-run mechanical commit

--- lib/Controller/ResourceServeController.php
+++ lib/Controller/ResourceServeController.php
@@ -44,6 +44,9 @@
  * @link      https://conduction.nl
  *
  * @spec openspec/changes/resource-serving/tasks.md
+ *
+ * SPDX-FileCopyrightText: 2026 Conduction B.V. <info@conduction.nl>
+ * SPDX-License-Identifier: EUPL-1.2
  */
 
 declare(strict_types=1);
